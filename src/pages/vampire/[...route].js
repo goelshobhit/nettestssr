6b975dd141e6073b6e437be71dd55e40da8ccb12
goelshobhit <itsshobhitgoel@gmail.com>
2021-12-20 20:00:37 +0530

    fix: webpack error

---
+++ src/pages/vampire/[...route].js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function ResolveRoute() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const lowerCaseRoutes = router.query.route.map(param => {
+      return `/${param.toLowerCase()}`;
+    });
+    const newRoute = lowerCaseRoutes.toString().replaceAll(',', '');
+    router.push(newRoute);
+  });
+
+  return <p>Redirecting...</p>;
+}
